2fb3c3d4ca5981ae84522204f0ec6e09f2876082
Mattias Holmlund <mattias@holmlund.se>
2019-08-20 08:45:57 +0200

    Print mean sample time

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Suite, Event } from 'benchmark'
+const prettyMs = require('pretty-ms')
 
 const suite = new Suite()
 
@@ -12,7 +13,15 @@ suite
     })
     // add listeners
     .on('cycle', (event: Event) => {
+        const stats = (event.target as any).stats
         console.log(String(event.target))
+        console.log(
+            'Mean: ' +
+                prettyMs(stats.mean * 1000, {
+                    formatSubMilliseconds: true,
+                    verbose: true,
+                }),
+        )
     })
     .on('complete', function(this: Suite) {
         console.log(
